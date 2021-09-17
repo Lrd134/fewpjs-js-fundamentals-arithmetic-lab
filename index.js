4118c2b8afd9b1a59ac47d80fdeacd2077ad9c1d
Lrd134 <larryc3200@gmail.com>
2021-09-16 20:51:10 -0400

    commit tree 170b6cb60ac438e99b5eaf14c0753a0bfa831fda

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+const random = Math.floor( Math.random()) + 1;
+let num3 = 4;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(10, 20);
